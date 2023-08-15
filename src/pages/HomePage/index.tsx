e9093113707bf68dbc9cc5b7b0e587cca9d8b747
lostintheway <sushilsampangrai@gmail.com>
2023-08-15 15:15:38 +0545

    fixed Name design

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -10,16 +10,16 @@ const MainContent = ({ slideRef }: Props) => {
     <div className="main-wrapper">
       <div className={"main-content"}>
         <div ref={slideRef} className="slide-up-container">
-          <h1>Mohan Rai</h1>
+          <h1 style={{ fontSize: 40, marginBottom: 0 }}>Mohan Rai</h1>
+          <h4 style={{ color: "grey", marginTop: 5 }}>Film Director</h4>
           <p>
             Hello, I'm reaching out to discuss potential collaboration
             opportunities as a fellow film developer. Let's explore how our
             shared passion can lead to exciting projects.
           </p>
-          Manjushree Thapa's latest novel is
-          <h4>All of Us in Our Own Lives</h4>
-          She is currently at work on a novel and a mixed-media work of
-          nonfiction.
+          My latest movie is
+          <h4>All of Us in Our Own Lives</h4>I am currently at work on a novel
+          and a mixed-media work of nonfiction.
         </div>
         {/* <div className="latest-work">
           <h4>Latest Work:</h4>
