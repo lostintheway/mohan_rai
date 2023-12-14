754853f5b47c75e2d939c38b885e2917453121e2
lostintheway <hypophantom@gmail.com>
2023-12-14 09:59:28 +0545

    contentadding

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,27 @@
 "use client";
+
 import { useRouter } from "next/navigation";
-import React from "react";
+import { useEffect } from "react";
 
-export default function Contact() {
-  useRouter().push("/");
+const MyComponent = () => {
+  const router = useRouter();
 
-  // redirect this page to "/"
-  // React.useEffect(() => {
+  useEffect(() => {
+    // Redirect to a different page after 3 seconds (3000 milliseconds)
+    const redirectTimer = setTimeout(() => {
+      router.push("/"); // Replace '/another-page' with the desired page path
+    }, 10);
 
-  // })
-  return <div></div>;
-}
+    // Clear the timer to avoid the redirect if the component unmounts
+    return () => clearTimeout(redirectTimer);
+  }, []); // The empty dependency array ensures this effect runs only once on mount
+
+  return (
+    <div>
+      <p>This is the initial content of the page.</p>
+      {/* You can add more content here */}
+    </div>
+  );
+};
+
+export default MyComponent;
